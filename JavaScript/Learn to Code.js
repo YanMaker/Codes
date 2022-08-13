8f517c133255e18d356f8b9ba125e01fb08d721a
YanMaker <94195591+YanMaker@users.noreply.github.com>
2022-08-13 11:14:21 -0400

    added color change

--- JavaScript/Learn to Code.js
+++ JavaScript/Learn to Code.js
@@ -1,10 +1,11 @@
 function myFunction(){
     document.getElementById("demo").innerHTML = "Hello, JS!";
     document.getElementById("demo").style.fontSize = "25px";
+    document.getElementById("demo").style.color = "green";
 }
 
 function myFunctionreverse(){
     document.getElementById("demo").innerHTML = "Demo for JS to change.";
     document.getElementById("demo").style.fontSize = "16px";
-    document.getElementById("demo").style.color
+    document.getElementById("demo").style.color = "black";
 }
