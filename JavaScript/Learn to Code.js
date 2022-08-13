e42664e97814169f28bd14db215e957a4d4ac343
YanMaker <94195591+YanMaker@users.noreply.github.com>
2022-08-13 10:52:24 -0400

    added reverse function

--- JavaScript/Learn to Code.js
+++ JavaScript/Learn to Code.js
@@ -1,3 +1,7 @@
 function myFunction(){
     document.getElementById("demo").innerHTML = "Hello, JS!"
 }
+
+function myFunctionreverse(){
+    document.getElementById("demo").innerHTML = "Demo for JS to change."
+}
