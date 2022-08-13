d3e801afcc612a7ddd8b3cd5a6a57ac44f74878c
YanMaker <94195591+YanMaker@users.noreply.github.com>
2022-08-13 11:07:50 -0400

    testing color

--- JavaScript/Learn to Code.js
+++ JavaScript/Learn to Code.js
@@ -1,7 +1,10 @@
 function myFunction(){
     document.getElementById("demo").innerHTML = "Hello, JS!";
+    document.getElementById("demo").style.fontSize = "25px";
 }
 
 function myFunctionreverse(){
     document.getElementById("demo").innerHTML = "Demo for JS to change.";
+    document.getElementById("demo").style.fontSize = "16px";
+    document.getElementById("demo").style.color
 }
